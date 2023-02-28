53e1691064b9c8e9362fe68316bc74dac86fc531
livings08 <marcob93@hotmail.com>
2023-02-28 14:36:04 +0100

    ADD Sessionstorage UPDATE: inlogpage
    ADD: Sessionstorage, takes the inloguser of the API and saves it in the sessionStore
    
    Update: InlogPage, adds a inlogform and creates when the user not found a user in the api

--- catch-em-all/src/app/components/login/login-form/login-form.component.ts
+++ catch-em-all/src/app/components/login/login-form/login-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Trainer } from 'src/app/models/trainer.model';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+  constructor(
+    private readonly router: Router,
+    private readonly loginService: LoginService
+  ) {}
 
+  ngOnInit(): void {}
+
+  public loginSubmit(loginForm: NgForm): void {
+    const { username } = loginForm.value;
+
+    this.loginService.login(username).subscribe({
+      next: (trainer: Trainer) => {
+        this.router.navigateByUrl('/catalogue');
+      },
+      error: () => {},
+    });
+  }
 }
